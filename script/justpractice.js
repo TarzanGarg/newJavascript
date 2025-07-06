eca0ce8740cd358feef9953bbe2ccaad1e883e57
TarzanGarg <tarzan6239@gmail.com>
2025-07-06 15:11:54 -0400

    update

---
+++ script/justpractice.js
@@ -0,0 +1,12 @@
+let a = 5;
+let b = 3;
+const sum = a + b;
+console.log (sum);
+
+let a1 = 8;
+let result;
+result = a1 + 18;
+result = result -4;
+result = result * 3;
+result = result / 2;
+alert (result);
